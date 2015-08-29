c3056231f1e526b8a641d52bbfbab88e5eb9661c
Ramsri Goutham Golla <ramsrigouthamg@gmail.com>
2015-08-29 00:07:18 -0700

    Committing code upto lesson 3.

---
+++ Udacity_Intro_Relational_Databases/vagrant/forum/forumdb.py
@@ -0,0 +1,47 @@
+#
+# Database access functions for the web forum.
+# 
+
+import time
+import psycopg2
+
+## Database connection
+#DB = []
+
+
+  
+
+
+## Get posts from database.
+def GetAllPosts():
+    '''Get all the posts from the database, sorted with the newest first.
+
+    Returns:
+      A list of dictionaries, where each dictionary has a 'content' key
+      pointing to the post content, and 'time' key pointing to the time
+      it was posted.
+    '''
+    db = psycopg2.connect("dbname=forum")
+    c = db.cursor()
+    query = "SELECT content, time FROM posts ORDER BY time DESC;"
+    c.execute(query)
+    rows = c.fetchall()
+    posts = [{'content': str(row[0]), 'time': str(row[1])} for row in rows]
+    #posts.sort(key=lambda row: row['time'], reverse=True)
+    db.close()
+    return posts
+
+## Add a post to the database.
+def AddPost(content):
+    '''Add a new post to the database.
+
+    Args:
+      content: The text content of the new post.
+    '''
+    t = time.strftime('%c', time.localtime())
+    db = psycopg2.connect("dbname=forum")
+    c = db.cursor()
+    c.execute("INSERT INTO posts (content) VALUES (%s)",(content,))
+    db.commit()
+    db.close()
+    
